87d553d4dc3f150b3a0e48f9c4cd8ecc3f6ac72e
Philipe Siqueira (WD5179) <philipe.siqueira@engie.com>
2020-01-07 09:34:38 -0300

    testes ok

---
+++ anagrams.js
@@ -0,0 +1,18 @@
+/*
+* Author: Philipe Siqueira
+* Versão 1.0
+*
+* Description: Find anagrams
+* Write a function that will find all the anagrams of a word from a list. 
+You will be given two inputs a word and an array with words. 
+You should return an array of all the anagrams or an empty array if there are none.
+*
+* Descrição: Encontrar anagramas
+* Escreva uma função que encontre todos os anagramas de uma palavra em uma lista. 
+Você receberá duas entradas, uma palavra e uma matriz com palavras. 
+Você deve retornar uma matriz de todos os anagramas ou uma matriz vazia.
+*/
+function anagrams(word, words) {
+  return words.filter( w => w.toLowerCase().split('').sort().join('') === word.toLowerCase().split('').sort().join(''))
+}
+console.log(anagrams('abba', ['aabb', 'abcd', 'bbaa', 'dada']));
